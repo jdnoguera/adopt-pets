26cedb0cea9ef218e90a350800856116fe7e9627
jdnoguera <josias17.noguera@gmail.com>
2021-08-17 07:52:37 -0400

    Se agrega el proyecto

---
+++ src/data/cats.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    name: 'Fish',
+    breed: 'tuxeado',
+    gender: 'male',
+    species: 'cat',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Fish',
+    breed: 'tuxeado',
+    gender: 'male',
+    species: 'cat',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Fish',
+    breed: 'tuxeado',
+    gender: 'male',
+    species: 'cat',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  },
+  {
+    name: 'Fish',
+    breed: 'tuxeado',
+    gender: 'male',
+    species: 'cat',
+    age: 20,
+    color: 'black/white',
+    weight: 13,
+    location: 'fourside',
+    notes: 'Sweet kitty. He loves getting his belly rubbed.'
+  }
+]
---
+++ src/data/dogs.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  },
+  {
+    name: 'Sheeba',
+    breed: 'collie',
+    gender: 'female',
+    age: 7,
+    color: 'black/white',
+    weight: 34,
+    location: 'fourside',
+    notes: 'Pure breed. Trained for competitions.'
+  }
+]
